401c80a3a8f9c3ee35f5161f526da80596c3c9a6
QuirkWorx <158528051+quirkyworks@users.noreply.github.com>
2024-04-25 08:44:17 -0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,14 @@
+# 80s computer nostalgia
+
+print("Hello, there!")
+
+name = input("What's your name?: ")
+
+print("Oh! that's your name,", name)
+
+print("Welcome to hainyberg!")
+print("Are you ready for your aventure?")
+
+answer = input("answer: ")
+
+print("Okay then! I'll take that as a yes!")
